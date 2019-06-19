6b017117e2e497ecccd17d14604a8e71fa909200
Krissy Hales <kristinakfriel@gmail.com>
2019-06-18 22:26:42 -0600

    changes

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,17 +3,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      array: ['animals', 'cats', 'dogs'],
+    }
+  }
+
+
   render() {
+    const list = this.state.array.map((item, i) => {
+      return <h2 key={i}>{item}</h2>
+    })
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div className="App">{list}</div>
     );
   }
 }
